f4ce3f1f96d665ac5d77672d71841169590f320a
PopovDS617 <popovds.bm@gmail.com>
2023-02-07 23:46:32 +0300

    fix: pre depl prep

--- server/app.ts
+++ server/app.ts
@@ -21,8 +21,21 @@ app.use(apiRoutes);
 
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
+  // res.setHeader('Access-Control-Allow-Credentials', true);
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, PUT, PATCH, OPTIONS, DELETE'
+  );
+
+  if (req.method === 'OPTIONS') {
+    return res.status(200).json({
+      body: 'OK',
+    });
+  }
   next();
 });
 
