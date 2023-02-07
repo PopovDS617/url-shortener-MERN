553667c43bd836ee5e9c638700a98e922073ae75
PopovDS617 <popovds.bm@gmail.com>
2023-02-07 23:05:42 +0300

    fix: pre depl prep

--- server/app.js
+++ server/app.js
@@ -4,6 +4,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const path_1 = __importDefault(require("path"));
 const routes_1 = __importDefault(require("./routes"));
 const body_parser_1 = __importDefault(require("body-parser"));
 const db_1 = __importDefault(require("./db/db"));
@@ -12,8 +13,13 @@ const dotenv_1 = __importDefault(require("dotenv"));
 dotenv_1.default.config();
 (0, db_1.default)();
 const app = (0, express_1.default)();
+app.get('/', (req, res) => {
+    res.sendFile('index.html', { root: path_1.default.join(__dirname, 'public') });
+});
+app.use(express_1.default.static('public'));
 app.use(body_parser_1.default.json());
 app.use((0, cors_1.default)());
 app.listen(process.env.PORT || 4000, () => {
+    console.log('app is running');
     (0, routes_1.default)(app);
 });
--- server/app.ts
+++ server/app.ts
@@ -12,7 +12,7 @@ const app = express();
 app.get('/', (req, res) => {
   res.sendFile('index.html', { root: path.join(__dirname, 'public') });
 });
-
+app.use(express.static('public'));
 app.use(bodyParser.json());
 
 app.use(cors());
