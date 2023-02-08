779681c900224f100c35e6689ad0913b90ea7a1b
Dmitry Popov <109096089+PopovDS617@users.noreply.github.com>
2023-02-08 11:19:50 +0300

    Update app.js

--- server/app.js
+++ server/app.js
@@ -18,19 +18,8 @@ app.get('/', (req, res) => {
 app.use(express_1.default.static('public'));
 app.use(body_parser_1.default.json());
 app.use(index_1.default);
-//app.use(cors());
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    // res.setHeader('Access-Control-Allow-Credentials', true);
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, OPTIONS, DELETE');
-    if (req.method === 'OPTIONS') {
-        return res.status(200).json({
-            body: 'OK',
-        });
-    }
-    next();
-});
+app.use(cors());
+
 app.listen(process.env.PORT || 4000, () => {
     console.log('app is running');
 });
