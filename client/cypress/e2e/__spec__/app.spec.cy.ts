1147c3444b6848242e95846558bebe5e5fce2346
PopovDS617 <popovds.bm@gmail.com>
2023-02-12 15:00:38 +0300

    test: add cypress tests

--- client/cypress/e2e/__spec__/app.spec.cy.ts
+++ client/cypress/e2e/__spec__/app.spec.cy.ts
@@ -7,11 +7,30 @@ describe("app e2e", () => {
   });
 
   it("should render input and button initially", () => {
-    cy.get("button");
-    cy.get("input");
+    cy.get("button").should("be.visible");
+    cy.get("input").should("be.visible");
   });
 
   it("should render a button that contains text - press", () => {
     cy.contains("button", "press");
   });
+
+  it("should render an input that is empty initially", () => {
+    cy.get("input").should("have.value", "");
+  });
+
+  it("should render an input that displays entered value", () => {
+    cy.get("input").type("test string");
+    cy.get("input").should("have.value", "test string");
+  });
+
+  it("should render a spinner after button has been clicked", () => {
+    cy.get("button").click();
+
+    cy.get("[data-testid='spinner']");
+  });
+  // it("should render a response section after button has been clicked", () => {
+  //   cy.get("button").click();
+  //   cy.wait(4000);
+  // });
 });
